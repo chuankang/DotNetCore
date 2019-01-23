98dfa04c6633d6d2b8190ce21b23f1bca208fb55
cksun <1037134100@qq.com>
2019-01-23 16:38:36 +0800

    csredis 发布订阅

--- OwnSpace/DotNetCore.Mvc/Controllers/RedisCoverController.cs
+++ OwnSpace/DotNetCore.Mvc/Controllers/RedisCoverController.cs
@@ -33,32 +33,28 @@ namespace DotNetCore.Mvc.Controllers
         }
 
         [HttpGet]
-        public JsonResult RedisTest(string name)
+        public JsonResult RedisTest()
         {
-            var x = 12;
-            //条件编译符号
-#if DEBUG2
-            x = 13;
-#elif Test
-            x = 15;
-#else
-            x = 19;
-#endif
-            ////普通模式
-            //var csredis = new CSRedis.CSRedisClient
-            //    ("127.0.0.1:6379,defaultDatabase=0,password=,poolsize=50,ssl=false,writeBuffer=10240,prefix=keyTest_");
+            //普通模式
+            //参数名           默认值         说明
+            //password          <空>        密码
+            //defaultDatabase   0           默认数据库
+            //poolsize          50          连接池大小
+            //preheat           true        预热连接
+            //ssl               false       是否开启加密传输
+            //writeBuffer       10240       异步方法写入缓冲区大小(字节)
+            //tryit             0           执行命令出错，尝试重试的次数
+            //name              <空>         连接名称，可以使用 Client List 命令查看
+            //prefix            <空>         key前辍，所有方法都会附带此前辍，csredis.Set(prefix + "key", 111);
+            //var csredis = new CSRedisClient(@"127.0.0.1:6380,defaultDatabase=0,password=,
+            //        poolsize=50,ssl=false,writeBuffer=10240,prefix=fund");
 
-            ////初始化RedisHelper
+            //////初始化RedisHelper
             //RedisHelper.Initialization(csredis);
 
-            var result = new CaptchaResult
-            {
-                CaptchaCode = "123",
-                CaptchaByteData = new byte[2]
-            };
             //设置值，默认永不过期
             //设置过期时间，redis-server 2.8+ 才支持
-            RedisHelper.Set("result", result);
+            //RedisHelper.Set("name", name,60);
             //异步操作
             //RedisHelper.SetAsync("async name", "ck async");
 
@@ -70,21 +66,27 @@ namespace DotNetCore.Mvc.Controllers
             //    redis[i] = new CSRedisClient(connectionString+ ",defualtDatabase=" + i);
             //}
 
+            //var cacheName = RedisHelper.Get("name");
+
+
+
+            ////发布
+            //RedisHelper.Publish("chan1", "1");
+            ////发布
+            //RedisHelper.Publish("chan1", "2");
+            ////发布
+            //RedisHelper.Publish("chan1", "3");
             //普通订阅
-            RedisHelper.Subscribe(
-                ("chan1", msg => Console.WriteLine(msg.Body)),
-                ("chan2", msg => Console.WriteLine(msg.Body)));
+            var sub = RedisHelper.Subscribe(("chan1", msg => GetName(msg.Body)));
+         
 
-            //发布
-            RedisHelper.Publish("chan1", "123123123");
+            ////使用管道模式，打包多条命令一起执行，从而提高性能。
+            //var ret1 = RedisHelper.StartPipe().Set("a", "1").Get("a").EndPipe();
+            //var ret2 = RedisHelper.StartPipe(p => p.Set("a", "1").Get("a"));
+            //var ret3 = RedisHelper.StartPipe().Get("b").Get("a").Get("a").EndPipe();
+            //var get = RedisHelper.Get<CaptchaResult>("name");
 
-            //使用管道模式，打包多条命令一起执行，从而提高性能。
-            var ret1 = RedisHelper.StartPipe().Set("a", "1").Get("a").EndPipe();
-            var ret2 = RedisHelper.StartPipe(p => p.Set("a", "1").Get("a"));
-            var ret3 = RedisHelper.StartPipe().Get("b").Get("a").Get("a").EndPipe();
-            var get = RedisHelper.Get<CaptchaResult>("name");
-
-            return null;
+            return Json("");
         }
 
         /// <summary>
@@ -94,7 +96,18 @@ namespace DotNetCore.Mvc.Controllers
         [HttpGet]
         public JsonResult RedisSubAndPub()
         {
+            //发布
+            RedisHelper.Publish("chan1", "4");
+            //发布
+            RedisHelper.Publish("chan1", "5");
+            //发布
+            RedisHelper.Publish("chan1", "6");
             return Json(null);
+        }
+
+        private static void GetName(string message)
+        {
+            var s = message;
         }
     }
 }
